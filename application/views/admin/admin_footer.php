01e8a3b82676da42470ac73c2a140062e3a3d87e
Yamesh1994 <mahule.yamesh@gmail.com>
2021-08-12 02:24:28 +0530

    Yamesh commit

--- application/views/admin/admin_footer.php
+++ application/views/admin/admin_footer.php
@@ -1,6 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
 <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
 </body>
-
+<b>This is clinet</b>
 
 </html>
